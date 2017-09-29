87f767796cb3ce71c1c55efdfd066102bcc83f21
avinash gupta <avinashgupta@avinashs-MacBook-Pro.local>
2017-09-29 12:16:19 +1000

    add

--- Example/index.android.js
+++ Example/index.android.js
@@ -4,50 +4,40 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ /**
+  * Sample React Native App
+  * https://github.com/facebook/react-native
+  * @flow
+  */
 
-export default class Example extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+ import React, {Component} from 'react';
+ import {AppRegistry, StyleSheet, View, Button} from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import ToastAndroid from 'react-native-toast';
+
+ const onButtonPress = () => {
+  ToastAndroid.show('Awesome', ToastAndroid.SHORT);
+  console.log("test");
+ };
+
+ export default class Example extends Component {
+   render() {
+     return (
+       <View style={styles.container}>
+         <Button title='Click' onPress={onButtonPress}/>
+       </View>
+     );
+   }
+ }
+
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+     backgroundColor: '#F5FCFF'
+   }
+ });
+
 
 AppRegistry.registerComponent('Example', () => Example);
