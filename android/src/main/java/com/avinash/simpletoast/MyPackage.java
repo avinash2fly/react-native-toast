4ac45c20b5812f6f98aa26fae0a1bfc8c3de57b3
avinash gupta <avinashgupta@avinashs-MacBook-Pro.local>
2017-09-29 12:17:19 +1000

    erro fix

--- android/src/main/java/com/avinash/simpletoast/MyPackage.java
+++ android/src/main/java/com/avinash/simpletoast/MyPackage.java
@@ -3,7 +3,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-
+import com.avinash.simpletoast.ToastModule;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
