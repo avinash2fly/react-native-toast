0a638b55acb31852bd9811eb74426fb53f637824
avinash gupta <avinashgupta@avinashs-MacBook-Pro.local>
2017-09-29 12:01:38 +1000

    example

--- Example/index.android.js
+++ Example/index.android.js
@@ -4,38 +4,50 @@
  * @flow
  */
 
- /**
-  * Sample React Native App
-  * https://github.com/facebook/react-native
-  * @flow
-  */
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
- import React, {Component} from 'react';
- import {AppRegistry, StyleSheet, View, Button} from 'react-native';
+export default class Example extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
 
- import MyModule from 'react-native-toast';
-
- const onButtonPress = () => {
-   MyModule.alert('Hello World');
- };
-
- export default class Example extends Component {
-   render() {
-     return (
-       <View style={styles.container}>
-         <Button title='Click' onPress={onButtonPress}/>
-       </View>
-     );
-   }
- }
-
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     justifyContent: 'center',
-     alignItems: 'center',
-     backgroundColor: '#F5FCFF'
-   }
- });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 AppRegistry.registerComponent('Example', () => Example);
